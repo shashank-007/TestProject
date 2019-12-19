6a55c0d5d2bb5b3160d364f9ff5202b2a0f14fa1
shashank-007 <shashank.vtalent@gmail.com>
2019-12-19 18:06:03 +0530

    Added Bal Class in Bussiness Access Layer

---
+++ TestProject/TestProject/BussinessAccessLayer/Bal.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TestProject.BussinessAccessLayer
+{
+    public class Bal
+    {
+        public int Addition()
+        {
+            int a = 10;
+            int b = 70;
+            int result = a + b;
+            return result;
+        }
+    }
+}
